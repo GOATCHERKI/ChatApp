368157591ba80c79053423307ca8e705a08b2e9b
Moahmed Zakaria Cherki <c.zikocherki@gmail.com>
2025-07-23 20:37:41 +0300

    frontend deployment

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideAnimationsAsync(),
-    provideRouter(routes),
+    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
